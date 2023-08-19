5d224c35529a5fdc82dfbb65b4bfd329bef21817
Emre Ozturk <88751167+EmreeOzturk@users.noreply.github.com>
2023-08-19 14:04:55 +0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import ModelX from "@/components/ui/ModelX";
 import ModelY from "@/components/ui/ModelY";
 
 const HomePage = () => {
-  console.log(" new env")
   return (
     <div>
       <ModelY />
